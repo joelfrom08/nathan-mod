1d4a9ada7e0616086977395aa6c7d5480b685072
joelfrom08 <dev.contact@petbyte.dev>
2023-05-21 20:32:49 +0200

    beloved wood - no trees, signs or crafting

--- src/main/java/joel/tutorialmod/data/ModWorldGenerator.java
+++ src/main/java/joel/tutorialmod/data/ModWorldGenerator.java
@@ -1,4 +1,24 @@
 package joel.tutorialmod.data;
 
-public class ModWorldGenerator {
+import joel.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModWorldGenerator extends FabricDynamicRegistryProvider {
+    public ModWorldGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup registries, Entries entries) {
+//future world gen
+    }
+
+    @Override
+    public String getName() {
+        return TutorialMod.MOD_ID;
+    }
 }
