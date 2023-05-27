10e1390182d1a51293d940a4bf404afe9a531089
joelr <joelrusch2008@gmail.com>
2023-05-27 21:53:54 +0200

    added ore generation & README.txt this is the last 1.19.3 build until full release

---
+++ src/main/java/joel/tutorialmod/world/gen/ModOreGeneration.java
@@ -0,0 +1,16 @@
+package joel.tutorialmod.world.gen;
+
+import joel.tutorialmod.world.ModPlacedFeatures;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.gen.GenerationStep;
+
+public class ModOreGeneration {
+    public static void generateOres() {
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
+                GenerationStep.Feature.UNDERGROUND_ORES, ModPlacedFeatures.NATHAN_ORE_PLACED_KEY);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
+                GenerationStep.Feature.UNDERGROUND_ORES, ModPlacedFeatures.NATHAN_ORE_TWO_PLACED_KEY);
+    }
+}
