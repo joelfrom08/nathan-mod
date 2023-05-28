f4ec61ebfcb332d03569d63e56504d5d55c8f6fe
joelfrom08 <dev.contact@petbyte.dev>
2023-05-28 14:20:33 +0200

    added toolset

--- README.txt
+++ README.txt
@@ -1,10 +1,12 @@
-This mod is for 1.19.3 fabric and built off the example mod fabric provides you, along with the 1.19.3-4 Fabric Modding Tutorial by
+This mod is for 1.19.4 fabric and built off the example mod fabric provides you, along with the 1.19.3-4 Fabric Modding Tutorial by
 youtube.com/@ModdingByKaupenjoe
 
 Known bugs:
 -Unable to craft planks
 -Unable to make charcoal
 -Doors drop two of themselves
+-Unable to craft tools
+-Some blocks are disabled
 
 Missing features:
 -Sign
@@ -12,7 +14,6 @@ Missing features:
 -Chest Boat
 -Nathan Dimension
 -Armor set
--Tool set
 -Nathan mob
 -Nathan grass
 -Nathan dirt
--- src/main/java/joel/tutorialmod/item/ModItems.java
+++ src/main/java/joel/tutorialmod/item/ModItems.java
@@ -1,11 +1,12 @@
 package joel.tutorialmod.item;
 
 import joel.tutorialmod.TutorialMod;
+import joel.tutorialmod.item.custom.ModAxeItem;
+import joel.tutorialmod.item.custom.ModHoeItem;
+import joel.tutorialmod.item.custom.ModPickaxeItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -20,6 +21,17 @@ public class ModItems {
     public static final Item RAW_NATHAN2 = registerItem("raw_beloved_ore_2",
             new Item(new FabricItemSettings()));
 
+    public static final Item NATHAN_SWORD = registerItem("beloved_sword",
+            new SwordItem(ModToolMaterials.NATHAN, 1, 2f, new FabricItemSettings()));
+    public static final Item NATHAN_PICKAXE = registerItem("beloved_pickaxe",
+            new ModPickaxeItem(ModToolMaterials.NATHAN, (1+1/2), 1.1f, new FabricItemSettings()));
+    public static final Item NATHAN_AXE = registerItem("beloved_axe",
+            new ModAxeItem(ModToolMaterials.NATHAN, 2, 2f, new FabricItemSettings()));
+    public static final Item NATHAN_SHOVEL = registerItem("beloved_shovel",
+            new ShovelItem(ModToolMaterials.NATHAN, 0, 0.2f, new FabricItemSettings()));
+    public static final Item NATHAN_HOE = registerItem("beloved_hoe",
+            new ModHoeItem(ModToolMaterials.NATHAN, 0, 0.2f, new FabricItemSettings()));
+
 
 
     private static Item registerItem(String name, Item item) {
@@ -31,6 +43,11 @@ public class ModItems {
         addToItemGroup(ModItemGroup.NATHAN, NATHAN2);
         addToItemGroup(ModItemGroup.NATHAN, RAW_NATHAN);
         addToItemGroup(ModItemGroup.NATHAN, RAW_NATHAN2);
+        addToItemGroup(ModItemGroup.NATHAN, NATHAN_SWORD);
+        addToItemGroup(ModItemGroup.NATHAN, NATHAN_PICKAXE);
+        addToItemGroup(ModItemGroup.NATHAN, NATHAN_AXE);
+        addToItemGroup(ModItemGroup.NATHAN, NATHAN_SHOVEL);
+        addToItemGroup(ModItemGroup.NATHAN, NATHAN_HOE);
     }
 
     private static void addToItemGroup(ItemGroup group, Item item) {
